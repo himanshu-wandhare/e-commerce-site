2e7f9669e26935a8d8ece296a637b92f5637bd65
Paju980 <himanshuwandhare13@gmail.com>
2024-05-27 14:23:11 +0530

    Add product-form and product action

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -1,5 +1,55 @@
 "use server";
 
-export async function addProduct(formData: FormData) {
-  console.log(formData);
+import db from "@/db";
+import fs from "fs/promises";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const fileSchema = z.instanceof(File, { message: "Required" });
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/")
+);
+
+const productSchema = z.object({
+  name: z.string().min(1),
+  priceInCents: z.coerce.number().min(1),
+  description: z.string().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "Required"),
+  image: imageSchema.refine((file) => file.size > 0, "Required"),
+});
+
+export async function addProduct(prevValue: unknown, formData: FormData) {
+  const result = productSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = result.data;
+
+  await fs.mkdir("products", { recursive: true });
+  const filePath = `products/${crypto.randomUUID()}-${data.file.name}`;
+  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+
+  await fs.mkdir("public/products", { recursive: true });
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
+  await fs.writeFile(
+    `public/${imagePath}`,
+    Buffer.from(await data.image.arrayBuffer())
+  );
+
+  await db.product.create({
+    data: {
+      name: data.name,
+      priceInCents: data.priceInCents,
+      description: data.description,
+      isAvaialableForPurchase: false,
+      filePath,
+      imagePath,
+    },
+  });
+
+  redirect("/admin/products");
 }
--- src/components/admin/products/product-form.tsx
+++ src/components/admin/products/product-form.tsx
@@ -1,38 +1,71 @@
-import { addProduct } from "@/actions/products";
+"use client";
+
+import { useFormState, useFormStatus } from "react-dom";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import PriceInCentsInput from "./price-in-cents-input";
+
+import { addProduct } from "@/actions/products";
+import { formatCurrency } from "@/lib/formatter";
+import { useState } from "react";
 
 export default function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<number>();
+
+  const [error, action] = useFormState(addProduct, {});
+
   return (
-    <form action={addProduct} className="space-y-8">
+    <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
         <Input type="text" name="name" id="name" />
+        {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price in Cents</Label>
-        <PriceInCentsInput
+        <Input
           type="number"
           name="priceInCents"
           id="priceInCents"
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
+        <div className="text-muted-foreground">
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+        {error.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
         <Textarea id="description" name="description" />
+        {error.description && (
+          <div className="text-destructive">{error.description}</div>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
         <Input type="file" name="file" id="file" />
+        {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
         <Input type="file" name="image" id="image" />
+        {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
-      <Button type="submit">Save</Button>
+      <SaveButton />
     </form>
+  );
+}
+
+function SaveButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Saving..." : "Save"}
+    </Button>
   );
 }
